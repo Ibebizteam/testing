b8a57dbec4dac4d53667be3f7a7ddaef7dce0649
HieuNguyen <hieu.nguyen@ibebiz.com>
2016-11-14 09:18:51 +0700

    Them moi controller testing

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -8,6 +8,10 @@ namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Add new
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
